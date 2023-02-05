5433b879f7d04740fdea139fda83508bfeb304b2
HaishengW <freizl@gmail.com>
2023-02-04 21:23:12 -0800

    add symbol in doc page

--- src/views/DocView.vue
+++ src/views/DocView.vue
@@ -31,13 +31,13 @@ if (id === "1") {
 let pageTitle = "";
 let fetchText = (d: SixtyFourGua) => [d.gua_ci];
 if (t === "yijing") {
-  pageTitle = `易經${pageSubTitle}`;
+  pageTitle = `易传${pageSubTitle}`;
   fetchText = (d) => [d.gua_ci, ...d.yao_ci];
 } else if (t === "tuan") {
-  pageTitle = `彖傳${pageSubTitle}`;
+  pageTitle = `彖传${pageSubTitle}`;
   fetchText = (d) => [d.tuan_ci];
 } else if (t === "xiang") {
-  pageTitle = `象傳${pageSubTitle}`;
+  pageTitle = `象传${pageSubTitle}`;
   fetchText = (d) => [d.da_xiang, ...d.xiao_xiang];
 }
 
@@ -45,7 +45,7 @@ const docs: Array<GuaDocViewObject> = data.map((d) => {
   return {
     id: d.id,
     name: d.name,
-    displayName: d.name + "卦",
+    displayName: d.symbol + d.name,
     url: "/gua/" + d.name,
     text: fetchText(d),
   };
@@ -54,11 +54,11 @@ const docs: Array<GuaDocViewObject> = data.map((d) => {
 
 <template>
   <h1>{{ pageTitle }}</h1>
-  <div class="shiyi-doc">
+  <div class="doc">
     <ol :start="startIndex">
       <li v-for="item in docs" v-bind:key="item.id">
-        <a :title="item.displayName" :href="item.url" class="name">
-          {{ item.name }}
+        <a :href="item.url" class="name">
+          {{ item.displayName }}
         </a>
         <ul>
           <li v-for="(v, index) in item.text" v-bind:key="index">
@@ -70,4 +70,9 @@ const docs: Array<GuaDocViewObject> = data.map((d) => {
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.doc .name {
+  font-size: 20pt;
+ }
+
+</style>
