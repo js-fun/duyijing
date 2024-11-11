2946dabba93f5fcf47db5b25b2b612a506d6cab8
HW home <freizl@gmail.com>
2024-11-10 22:47:27 -0800

    Fixes warning and format

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,30 +10,32 @@ const xianTianGuas = XianTian.reverse();
 <template>
   <div class="six-four">
     <table class="table">
-      <tr>
-        <td
-          v-for="item in xianTianGuas"
-          v-bind:key="item.id"
-          class="xian-tian-gua-container"
-        >
-          <EightGuaItemSmall :id="item.id" />
-        </td>
-        <td class="xian-tian-gua-container">
-          <div class="xian-tain-gua-action">
-            <span class="xian-tain-gua-note">←上卦</span>
-            <span class="xian-tain-gua-note">↓下卦</span>
-          </div>
-        </td>
-      </tr>
+      <tbody>
+        <tr>
+          <td
+            v-for="item in xianTianGuas"
+            v-bind:key="item.id"
+            class="xian-tian-gua-container"
+          >
+            <EightGuaItemSmall :id="item.id" />
+          </td>
+          <td class="xian-tian-gua-container">
+            <div class="xian-tain-gua-action">
+              <span class="xian-tain-gua-note">←上卦</span>
+              <span class="xian-tain-gua-note">↓下卦</span>
+            </div>
+          </td>
+        </tr>
 
-      <tr v-for="(items, index) in itemss" v-bind:key="index">
-        <td v-for="item in items" v-bind:key="item.id">
-          <GuaItemSmall :id="item.id" />
-        </td>
-        <td class="xian-tian-gua-container">
-          <EightGuaItemSmall :id="xianTianGuas[index].id" />
-        </td>
-      </tr>
+        <tr v-for="(items, index) in itemss" v-bind:key="index">
+          <td v-for="item in items" v-bind:key="item.id">
+            <GuaItemSmall :id="item.id" />
+          </td>
+          <td class="xian-tian-gua-container">
+            <EightGuaItemSmall :id="xianTianGuas[index].id" />
+          </td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
