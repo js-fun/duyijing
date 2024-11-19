a5e737882fad65bc908096d0df293f312a251a34
HW home <freizl@gmail.com>
2024-11-18 23:27:27 -0800

    change to simplified version

--- src/services/Yijing.ts
+++ src/services/Yijing.ts
@@ -1,11 +1,11 @@
 import type { SixtyFourGua, XianTian8Gua } from "@freizl/yijing";
-import YData from "@freizl/yijing/zh-TW/64gua.json";
-import XianTianData from "@freizl/yijing/zh-TW/xian-tian-8gua.json";
-import XiCiData from "@freizl/yijing/zh-TW/xi-ci.json";
-import WenYanData from "@freizl/yijing/zh-TW/wen-yan.json";
-import ShuoGuaData from "@freizl/yijing/zh-TW/shuo-gua.json";
-import XuGuaData from "@freizl/yijing/zh-TW/xu-gua.json";
-import ZaGuaData from "@freizl/yijing/zh-TW/za-gua.json";
+import YData from "@freizl/yijing/zh-CN/64gua.json";
+import XianTianData from "@freizl/yijing/zh-CN/xian-tian-8gua.json";
+import XiCiData from "@freizl/yijing/zh-CN/xi-ci.json";
+import WenYanData from "@freizl/yijing/zh-CN/wen-yan.json";
+import ShuoGuaData from "@freizl/yijing/zh-CN/shuo-gua.json";
+import XuGuaData from "@freizl/yijing/zh-CN/xu-gua.json";
+import ZaGuaData from "@freizl/yijing/zh-CN/za-gua.json";
 
 export const Y = YData;
 export const XianTian = XianTianData;
@@ -41,13 +41,13 @@ export function getEightGuaData(key: string) {
 }
 
 export const navs = [
-  { url: "/", title: "易經" },
-  { url: "/doc/yijing/1", title: "易經上" },
-  { url: "/doc/yijing/2", title: "易經下" },
-  { url: "/doc/tuan/1", title: "彖傳上" },
-  { url: "/doc/tuan/2", title: "彖傳下" },
-  { url: "/doc/xiang/1", title: "象傳上" },
-  { url: "/doc/xiang/2", title: "象傳下" },
+  { url: "/", title: "易经" },
+  { url: "/doc/yijing/1", title: "易传上" },
+  { url: "/doc/yijing/2", title: "易传下" },
+  { url: "/doc/tuan/1", title: "彖传上" },
+  { url: "/doc/tuan/2", title: "彖传下" },
+  { url: "/doc/xiang/1", title: "象传上" },
+  { url: "/doc/xiang/2", title: "象传下" },
   { url: "/doc/xi-ci/1", title: XiCi.title + "上" },
   { url: "/doc/xi-ci/2", title: XiCi.title + "下" },
   { url: "/doc/wen-yan", title: WenYan.title },
