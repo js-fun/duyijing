62a341fbdfbb616811268a2ddaa6053624d17988
HW home <freizl@gmail.com>
2025-11-19 20:42:19 -0800

    upgrade to node20

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? [["github"], ["line"]] : "html",
+  reporter: process.env.CI ? [["github"], ["line"], ["html"]] : "html",
   use: {
     baseURL: "http://127.0.0.1:4173",
     trace: "on-first-retry",
